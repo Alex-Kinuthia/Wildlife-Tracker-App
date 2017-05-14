ac9bb0b8090e6c9e1c25770252ef73f7255f7c46
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-05-14 11:55:56 +0300

    added content

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -26,49 +26,23 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      // model.put("animals", Animal.all());
-      // model.put("endangeredAnimals", EndangeredAnimal.all());
-      // model.put("sightings", Sighting.all());
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+
+        get("/contact", (request, response) -> {
+          Map<String, Object> model = new HashMap<String, Object>();
+          model.put("template", "templates/contact.vtl");
+          return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
     get("/animal/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("animals", Animal.all());
-      // model.put("endangeredAnimals", EndangeredAnimal.all());
+      model.put("endangeredAnimals", EndangeredAnimal.all());
       model.put("template", "templates/animal-form.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-
-    get("/animal/:id", (request, response) -> {
-  Map<String, Object> model = new HashMap<String, Object>();
-  Animal animal = Animal.find(Integer.parseInt(request.params("id")));
-  model.put("animal", animal);
-  model.put("template", "templates/animal.vtl");
-  return new ModelAndView(model, layout);
-}, new VelocityTemplateEngine());
-
-post("/animal/new", (request, response) -> {
-  Map<String, Object> model = new HashMap<String, Object>();
-  boolean endangered = request.queryParamsValues("endangered")!=null;
-  if (endangered) {
-    String name = request.queryParams("name");
-    String health = request.queryParams("health");
-    String age = request.queryParams("age");
-    EndangeredAnimal endangeredAnimal = new EndangeredAnimal(name, health, age);
-    endangeredAnimal.save();
-    model.put("animals", Animal.all());
-    model.put("endangeredAnimals", EndangeredAnimal.all());
-  } else {
-    String name = request.queryParams("name");
-    Animal animal = new Animal(name);
-    animal.save();
-    model.put("animals", Animal.all());
-    model.put("endangeredAnimals", EndangeredAnimal.all());
-  }
-  response.redirect("/");
-    return null;
-  });
 }
 }
